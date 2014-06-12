48829f18b2b50f5468c02fad0e1ca820f3425c0f
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2014-06-12 11:59:27 +0900

    Add coretemp

---
+++ coretemp.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+SECONDS=`date '+%s'`
+
+NAME='coretemp.cpu'
+TEMP=`cat /sys/devices/platform/coretemp.0/temp1_input`
+VALUE=`echo "scale=2; ${TEMP} / 1000" | bc`
+echo "${NAME}\t${VALUE}\t${SECONDS}"
+
+NAME='coretemp.core0'
+TEMP=`cat /sys/devices/platform/coretemp.0/temp2_input`
+VALUE=`echo "scale=2; ${TEMP} / 1000" | bc`
+echo "${NAME}\t${VALUE}\t${SECONDS}"
+
+NAME='coretemp.core1'
+TEMP=`cat /sys/devices/platform/coretemp.0/temp3_input`
+VALUE=`echo "scale=2; ${TEMP} / 1000" | bc`
+echo "${NAME}\t${VALUE}\t${SECONDS}"
