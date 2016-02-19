8c22c298224db037fb73016d631b532962fae223
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2016-02-20 00:19:05 +0900

    Update temp.sh

--- raspberrypi/temp.sh
+++ raspberrypi/temp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 SECONDS=`date '+%s'`
 
-NAME='temp.cpu'
+NAME='temperature.cpu'
 TEMP=`cat /sys/class/thermal/thermal_zone0/temp`
 VALUE=`echo "scale=3; ${TEMP} / 1000" | bc`
 echo "${NAME}\t${VALUE}\t${SECONDS}"
